12cfb538f8c8b76f246da4ddd2eccd02c59990e6
adamY97 <adam24891@gmail.com>
2021-10-22 12:44:23 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/RetoCloudClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoCloudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoCloudClientApplication.class, args);
+	}
+
+}
